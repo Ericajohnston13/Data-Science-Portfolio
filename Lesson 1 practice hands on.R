a8fed467f52257d60750ee5283ca3702478e4f3f
Ericajohnston13 <99776518+Ericajohnston13@users.noreply.github.com>
2023-10-16 13:06:30 -0400

    Add files via upload

---
+++ Lesson 1 practice hands on.R
@@ -0,0 +1,21 @@
+install.packages("tidyr")
+library("tidyr")
+
+
+FakeNews$StoryType = "Fake"
+
+
+
+FakeNews1 <- FakeNews[, 2:4]
+
+
+library("tidyr")
+FakeNews2 <- separate(FakeNews1, url, c("Website", "Domain"), sep="_")
+
+{r}
+FakeNews3 <- unite(FakeNews2, FullSiteName, Website, Domain, sep = "_")
+
+
+FakeNews4 <- FakeNews3[1:10,]
+
+
